42738eb4d301c7aeb50d820e6961cf9c5382e271
Esten Rye <esten.rye@ryezone.com>
2024-07-07 22:11:30 -0500

    fix linting issues in valid terraform

--- tf-tests/validly-formatted/head.tf
+++ tf-tests/validly-formatted/head.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    http = {
+      source = "hashicorp/http"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "http" {
+  # Configuration options
+}
+
 data "http" "example_head" {
   url    = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
   method = "HEAD"
